ad62575856334b6af55810d9535fab56f087e4a6
SUP3RS0N1C <sputnik1957@sookmyung.ac.kr>
2019-11-28 16:09:24 +0900

    02-doing

--- main.c
+++ main.c
@@ -3,21 +3,78 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct student {
-	int ID;
-	char name[10];
-	double grade;
-};
-
-int main(int argc, char *argv[]) {
-	struct student s1 = {24, "chPark", 4.3};
-	s.ID = 123456;
-	strcpy(s1.name, "Juyeop");
-	s1.grade = 4.2;
+int is_whitespace(char c)
+{
+	if (c==' ' ||
+		c=='\r' ||
+		c=='\n' ||
+		c=='\t' ||
+		c=='(')
+	{
+		return 1;
+	}
 	
-	printf("ID : %d\n", s1.ID);
-	printf("name  : %s\n", s1.name);
-	printf("grade : %f\n", s1.grade);
+	return 0;
+}
+
+int fget_word(FILE* fp, char* word)
+{
+	int cnt;
+	char c;
+	//무의미한 글자 건너뛰기  
+	while((c=fgetc(fp)) != EOF)
+	{
+		if(is_whitespace(c) == 0)
+			break;
+	}
+	if (c==EOF)
+	{
+		return 0;
+	}
+
+	cnt = 0;
+	word[cnt++] = c;
+	word[cnt] = '\0';
+	
+	while((word[cnt] = fgetc(fp)) != EOF)
+	{
+		if (is_whitespace(word[cnt])==1)
+		{
+			word[cnt] ='\0';
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt;
+}
+
+int main(int argc, char *argv[]) 
+{
+	FILE *fp;
+	char filepath[100];
+	char word[100];
+	
+	//file open
+	printf("input the file path : "); 
+	scanf("%s",filepath/*파일패스는 포인터에해당*/);
+	fp = fopen(filepath, "r");
+	
+	if (fp == NULL) //방어코드  
+	{
+		printf("file path is wrong! %s\n", filepath);
+		return -1;
+	}
+	
+
+	//word reading & analysis
+	while(fget_word(fp,word) != 0)
+	{
+		printf("%s\n", word);
+	}//검증용 임시코드  
+	
+	//output
+	fclose(fp);
 	
 	return 0;
 }
