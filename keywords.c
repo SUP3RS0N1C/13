66a542f1d6196d096df4607a84d10d09bacdd0f2
SUP3RS0N1C <sputnik1957@sookmyung.ac.kr>
2019-12-05 15:15:33 +0900

    04

--- keywords.c
+++ keywords.c
@@ -1,11 +1,12 @@
-#define MAX_NAME 30 //매크로  
+#define MAX_NAME 30 //매크로 
+#define N_KEYS 14
 
 struct keytab{ //구조체 틀  
 	int nums; //몇번나오는지 카운트
 	char name[MAX_NAME]; //가장긴키워드의 길이  
 };
 
-struct keytab keywrds[13/*원래는 매크로로 들어가야함*/] ={
+struct keytab keywrds[N_KEYS] ={
 	{0, "int"},
 	{0, "float"},
 	{0, "return"},
@@ -18,13 +19,14 @@ struct keytab keywrds[13/*원래는 매크로로 들어가야함*/] ={
 	{0, "void"},
 	{0, "default"},
 	{0, "char"},
-	{0, "do"}
+	{0, "do"},
+	{0, "scanf"}
 };
 
 void count_word(char *word){
 	int i;
 	//repeat each keywords
-	for (i=0;i<13;i++)
+	for (i=0;i<N_KEYS;i++)
 	{	//if word and i'th keyword is same
 		if(strncmp(word, keywrds[i].name, strlen(keywrds[i].name)/*keywrds문자열의 길이*/)==0/*n개까지 비교*/){
 			//nums++;
@@ -37,7 +39,7 @@ void print_word()//키워드의 등장횟수를 프린트
 {
 	int i;
 	//i번쨰 키워드에 대해서
-	for(i=0;i<13;i++)
+	for(i=0;i<N_KEYS;i++)
 	{
 		//printf(키워드이름:빈도수);
 		printf("%s : %i\n", keywrds[i].name, keywrds[i].nums);
